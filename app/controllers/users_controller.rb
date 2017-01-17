05716736cec9f3c6aaad394180723ec1c3d10924
Sunil <sunil@SK-Mac-mini.local>
2017-01-17 19:27:48 +0530

    Finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def create
+    @user = User.new(user_params) # @user = User.new(params[:user]) 
+    if @user.save
+      flash[:success] = "Welcome to the Brillio App!"
+      redirect_to @user # equivalent to redirect_to user_url(@user)
+      #Handle a sucessful signup
+    else 
+      render 'new'
+    end
   end
+
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+ private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
